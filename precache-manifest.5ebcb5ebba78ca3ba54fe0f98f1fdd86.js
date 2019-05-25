9d9bb55ca5fac3d9b348323797683c6e4aca8005
yogesh kumar <yogeshkumar87@hotmail.com>
2019-05-25 17:55:11 +0530

    Updates

---
+++ precache-manifest.5ebcb5ebba78ca3ba54fe0f98f1fdd86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2146bcfd9cd198ff1e07f314593ffc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e42d97b691125abc1eed",
+    "url": "/static/css/2.4c9116bc.chunk.css"
+  },
+  {
+    "revision": "bd29547a800df4de62f4",
+    "url": "/static/css/main.88197e76.chunk.css"
+  },
+  {
+    "revision": "e42d97b691125abc1eed",
+    "url": "/static/js/2.c75ecacb.chunk.js"
+  },
+  {
+    "revision": "bd29547a800df4de62f4",
+    "url": "/static/js/main.41366b11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
